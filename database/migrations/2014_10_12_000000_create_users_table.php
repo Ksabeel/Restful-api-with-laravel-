b1d5466eb7874c9362472f26cc65f283a1af7749
Sabeel Abid <sabeelali13@gmail.com>
2018-06-03 04:27:58 +0500

    implementing the migration for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('verified')->default(0);
+            $table->string('verification_token')->nullable();
+            $table->sting('admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
