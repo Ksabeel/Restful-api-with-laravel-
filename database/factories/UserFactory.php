5eef9c3689db212556ad23a30fcf072cba0ceb45
Sabeel Abid <sabeelali13@gmail.com>
2018-06-03 05:42:48 +0500

    creating the factory for products

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,17 @@ $factory->define(App\User::class, function (Faker $faker) {
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'email' => $faker->paragraph(1),
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(3),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->boolean,
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.png']),
+        'seller_id' => App\User::inRandomOrder()->first()->id,
     ];
 });
